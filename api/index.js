c538358bbe028437f316a1266b96b9e337f4bd9b
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-25 14:09:55 +0530

    Feat: task push to queue for an endpoint

---
+++ api/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { client, pushToTaskQueue } from "../queue/redisQueue";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(201).json({
+    msg: `Home route`,
+  });
+});
+
+app.post("/api/stock/update", async (req, res) => {
+  try {
+    const { symbol, interval } = req.body;
+
+    if (!symbol || !interval) {
+      return res.status(403).json({
+        msg: `Please provide required details.`,
+      });
+    }
+
+    const task = {
+      stockType: "stock_price_update",
+      payload: {
+        symbol: symbol.toUpperCase(),
+        interval: interval,
+      },
+    };
+
+    await pushToTaskQueue(task);
+
+    return res.status(201).json({
+      msg: `Pushed to Queues.`,
+      task,
+    });
+  } catch (err) {
+    console.error(`Error while pushing to queue.`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`api server running on 3000`);
+});
