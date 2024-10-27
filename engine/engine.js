d67a000ad9e210eb3ad916421b901ece4100a63c
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-27 10:01:16 +0530

    Feat: change binance api from rest to ws

--- engine/engine.js
+++ engine/engine.js
@@ -4,6 +4,7 @@ import {
   popFromTaskQueue,
   pushToDoneTaskQueue,
 } from "../queue/redisQueue.js";
+import { WebSocket } from "ws";
 
 /*
  * Pop from queue.
@@ -12,49 +13,70 @@ import {
  */
 
 // to get crypto price with rest-api of binance
-async function getCryptoPrice(symbolOfCrypto) {
-  try {
-    const response = await axios.get(
-      `https://api.binance.com/api/v3/ticker/price?symbol=${symbolOfCrypto}`
-    );
-    return response.data.price;
-  } catch (error) {
-    console.error(
-      `Error fetching the stock price of ${symbolOfCrypto}: ${error}`
-    );
-    return null;
-  }
-}
+async function getCryptoPrice(task, reconnectDelay = 3000) {
+  const { symbol } = task.payload;
+  const wsUrl = `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@trade`;
+  console.log(`Connecting to WebSocket URL: ${wsUrl}`);
+  const ws = new WebSocket(wsUrl);
 
-async function doTheTask(task) {
-  try {
-    console.log(`Starting to process the task: ${JSON.stringify(task)}`);
-    const { symbol } = task.payload;
+  ws.on("open", () => {
+    console.log("Successfully connected to wss api");
+    reconnectDelay = 3000;
+  });
 
-    console.log(`Getting price of ${symbol}`);
-    const cryptoPrice = await getCryptoPrice(symbol);
-
-    if (!cryptoPrice) {
-      console.error(`Failed to get price of ${symbol}, skipping task.`);
-      return;
-    }
-
-    console.log(`Crypto price of ${symbol} is ${cryptoPrice}`);
-
-    const doneTask = {
+  // let lastPrice = null;
+  ws.on("message", async (data) => {
+    const trade = JSON.parse(data);
+    let cryptoPrice = parseFloat(trade.p);
+    // if (lastPrice === null || cryptoPrice !== lastPrice) {
+    // lastPrice = cryptoPrice;
+    let doneTask = {
       symbol: symbol,
       stockPrice: cryptoPrice,
       completedAt: new Date().toISOString(),
     };
-
     await pushToDoneTaskQueue(doneTask);
-    console.log(
-      `Successfully performed task and pushed to done queue from engine`
-    );
-  } catch (error) {
-    console.error(`Error while performing the task in engine`);
-  }
+    // } else {
+    //   console.log(`price didn't changed`);
+    // }
+    console.log(`Real-time price of ${symbol}: ${cryptoPrice}`);
+  });
+
+  ws.on("error", (error) =>
+    console.error(`Error getting price of ${symbol}: ${error}`)
+  );
+
+  ws.on("close", () => {
+    console.log(`Connection closed with wss api for ${symbol} Reconnecting...`);
+    setTimeout(() => {
+      getCryptoPrice(task, Math.min(reconnectDelay * 2, 6000));
+    }, reconnectDelay);
+  });
 }
+
+// async function doTheTask(task) {
+//   try {
+//     console.log(`Starting to process the task: ${JSON.stringify(task)}`);
+//     const { symbol } = task.payload;
+
+//     console.log(`Getting price of ${symbol}`);
+//     // const cryptoPrice = await getCryptoPrice(symbol);
+//     getCryptoPrice(symbol);
+
+//     const doneTask = {
+//       symbol: symbol,
+//       stockPrice: cryptoPrice,
+//       completedAt: new Date().toISOString(),
+//     };
+
+//     await pushToDoneTaskQueue(doneTask);
+//     console.log(
+//       `Successfully performed task and pushed to done queue from engine`
+//     );
+//   } catch (error) {
+//     console.error(`Error while performing the task in engine`);
+//   }
+// }
 
 async function startEngine() {
   console.log(`Connecting to Redis and starting engine...`);
@@ -64,8 +86,7 @@ async function startEngine() {
     try {
       console.log(`Task fetched from TaskQueue: ${JSON.stringify(task)}`);
       console.log(`Processing a new task...`);
-      await doTheTask(task);
-      console.log(`Finished processing task: ${JSON.stringify(task)}`);
+      await getCryptoPrice(task);
     } catch (error) {
       console.error(`Error in startEngine: ${error.message}`);
     }
